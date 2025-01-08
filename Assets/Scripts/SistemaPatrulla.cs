a507efa4afa3702b4290a84d027066f2d61e8322
DESKTOP-GV16N45\Alumno <martinawang03@gmail.com>
2025-01-08 18:15:25 +0100

    clase de 8/1/25

--- Assets/Scripts/SistemaPatrulla.cs
+++ Assets/Scripts/SistemaPatrulla.cs
@@ -1,26 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SistemaPatrulla : MonoBehaviour
 {
     [SerializeField] private Transform ruta;
 
-    private List<Transform> listadosPuntos= new List<Transform>();  
-    // Start is called before the first frame update
+    private NavMeshAgent agent;
 
-    private Transform[] array= new Transform[2];
-    void Start()
+    private List<Transform> listadosPuntos= new List<Transform>();
+
+    private int indiceDestinoActual = 0; //MArca el punto al cual debo ir.
+
+    private Transform destinoActual; //Marco el destino al cual debo ir.
+
+    private void Awake()
     {
-        foreach(Transform item in ruta)
+        agent = GetComponent<NavMeshAgent>();
+        foreach(Transform t in ruta)
         {
-            Debug.Log(item.name);
+            // Añado todos los puntos de ruta al listado.
+
+            listadosPuntos.Add(t);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private Transform[] array= new Transform[2];
+    private void Start()
     {
-        
+        StartCoroutine(PatruillarYEsperar());
     }
+
+    private IEnumerator PatruillarYEsperar()
+    {
+        //Por siempre
+        while(true)
+        {
+
+            CalcularDestino(); //Tendré que calcular el nuevo destido
+            
+            agent.SetDestination(destinoActual.position);
+          yield return new WaitUntil(() => agent. remainingDistance<=0); // () ES: expresión lambda (MÉTODO ANONIMO) 
+
+        }
+    }
+
+    private void CalcularDestino()
+    {
+        indiceDestinoActual++;
+
+        //Si nos hemos quedados sin puntos...
+        if(indiceDestinoActual> listadosPuntos.Count)
+        {
+            indiceDestinoActual = 0; 
+        }
+        destinoActual = listadosPuntos[indiceDestinoActual];
+    }
+    
 }
