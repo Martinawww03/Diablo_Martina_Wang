2e8a7bcd9e823f32b78f2fa0aa70911b14e9ddab
DESKTOP-GV16N45\Alumno <martinawang03@gmail.com>
2025-01-20 21:20:18 +0100

    minimapa

---
+++ Assets/Scripts/CamaraMinimapa.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraMinimapa : MonoBehaviour
+{
+
+    [SerializeField] private Player player;
+
+    private Vector3 distanciaAPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Calculo la distancia inicial que hay entre el player y yo (cámara)
+        distanciaAPlayer = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() // el último metodo funciona
+    {
+        transform.position = player.transform.position + distanciaAPlayer;
+    }
+}
