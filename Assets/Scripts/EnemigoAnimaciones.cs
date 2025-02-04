48f313d333aa443099027e42cfd53432b78a124c
Martinawww03 <martinawang03@gmail.com>
2025-02-04 14:49:26 +0100

    animaciones

---
+++ Assets/Scripts/EnemigoAnimaciones.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemigoAnimaciones : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim= GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("velocity", agent.velocity.magnitude / agent.speed);
+    }
+}
